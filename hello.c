ceeabbc10aefd99fa11d93b242765704d44d4500
Gabe-Ramsey <54554902+Gabe-Ramsey@users.noreply.github.com>
2019-08-29 15:53:33 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gabriel Ramsey and Ritvik Handa
+ * Date: 2019/27/09
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gabe Ramsey and Ritvik Handa!\n");
+  printf("Physics\n");
+  return 0;
+}
